2f1bf6607e327c33c99055fdf65dc3161c617d24
giuseppelt <giuseppelt@users.noreply.github.com>
2017-05-07 22:17:57 +0200

    Fixed windows param type

--- windows/OpenUrlExtProxy.js
+++ windows/OpenUrlExtProxy.js
@@ -1,6 +1,7 @@
 cordova.commandProxy.add("OpenUrlExt", {
 	open: function (successCallback, errorCallback, params) {
-		Windows.System.Launcher.launchUriAsync(params[0]).done(
+		var uri = new Windows.Foundation.Uri(params[0]);
+		Windows.System.Launcher.launchUriAsync(uri).done(
         function (success) {
             if (success) { 
             	console.log("page opened correctly"); 
